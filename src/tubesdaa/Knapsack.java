2d18739545dc7f60954bd8133e2f5899e21a91ef
geryn25 <35487884+geryn25@users.noreply.github.com>
2019-04-27 19:48:56 +0700

    Initial commit

---
+++ src/tubesdaa/Knapsack.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubesdaa;
+
+import java.util.Comparator;
+
+/**
+ *
+ * @author geryn
+ */
+public class Knapsack {
+    private int weight;
+    private int profit;
+    private double density;
+
+    public Knapsack(int weight, int profit) {
+        this.weight = weight;
+        this.profit = profit;
+        this.density=(double) this.profit / this.weight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getProfit() {
+        return profit;
+    }
+
+    public void setProfit(int profit) {
+        this.profit = profit;
+    }
+
+    public double getDensity() {
+        return density;
+    }
+
+    public void setDensity(double density) {
+        this.density = density;
+    }
+    
+    @Override
+    public String toString() {
+        return "Weight  :"+getWeight()+"\n"
+                + "Profit  :"+getProfit()+"\n"
+                + "Density :"+getDensity();
+    }
+    
+    public static Comparator<Knapsack> weightComparator = new Comparator<Knapsack>() {
+        @Override
+        public int compare(Knapsack o1, Knapsack o2) {
+            return o1.weight-o2.weight;
+        }
+    };
+    public static Comparator<Knapsack> profitComparator = new Comparator<Knapsack>() {
+        @Override
+        public int compare(Knapsack o1, Knapsack o2) {
+            return o2.profit-o1.profit;
+        }
+    };
+    public static Comparator<Knapsack> densityComparator=new Comparator<Knapsack>() {
+        @Override
+        public int compare(Knapsack o1, Knapsack o2) {
+            return (int)(o2.density-o1.density);
+        }
+    };
+    
+    
+}
+
---
+++ src/tubesdaa/TUBESDAA.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubesdaa;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+/**
+ *
+ * @author geryn
+ */
+public class TUBESDAA {
+
+    /**
+     * @param args the command line arguments
+     */
+    static ArrayList<Knapsack> knapsack=new ArrayList<Knapsack>();
+    public static void main(String[] args) throws IOException {
+        inputData(knapsack);
+        Scanner reader=new Scanner(System.in);
+        System.out.print("\n Masukkan kapasitas :");
+        int kapasitas=reader.nextInt();
+        int num=0;
+        do {
+            mainmenu();
+            num=reader.nextInt();
+            pilihan(num,kapasitas);
+            
+        }while (num!=0);
+        
+        
+        
+    }
+    public static void inputData(ArrayList<Knapsack> k) {
+        Scanner reader=new Scanner(System.in);
+        System.out.print("Masukkan Banyak data :");
+        int jum= reader.nextInt();
+        for (int i=0;i<jum;i++) {
+            System.out.println("data ke "+(i+1)+" :");
+            System.out.print("Weight :");
+            int weight=reader.nextInt();
+            System.out.print("Profit :");
+            int profit=reader.nextInt();
+            knapsack.add(new Knapsack(weight,profit));
+        }
+    }
+    
+    public static void greedy(int num,int k) throws IOException {
+        if (num==1) {
+            Collections.sort(knapsack,Knapsack.weightComparator);
+        }else if (num==2) {
+            Collections.sort(knapsack,Knapsack.profitComparator);
+        } else {
+            Collections.sort(knapsack,Knapsack.densityComparator);
+        }
+        System.out.println("Data diurut Berdasarkan min Weight");
+        for (int i=0;i<knapsack.size();i++) {
+            System.out.println("Data ke "+(i+1));
+            System.out.println(knapsack.get(i));
+            System.out.println("");
+        }
+        int kTemp=0;
+        int pTemp=0;
+        int i=0;
+        while (kTemp<=k  && kTemp+knapsack.get(i).getWeight()<=k) {
+            kTemp=kTemp+knapsack.get(i).getWeight();
+            pTemp=pTemp+knapsack.get(i).getProfit();
+            i++;
+        }
+        System.out.println("\nKapasitas : "+kTemp);
+        System.out.println("Profit    : "+pTemp);
+        
+        System.in.read();
+        
+    }
+    public static void mainmenu(){
+        System.out.println("\n>> Menu Greedy:");
+        System.out.println("   1. Greedy by Weight");
+        System.out.println("   2. Greedy by Profit");
+        System.out.println("   3. Greedy by Density");
+        System.out.print(">> Masukkan pilihan anda :");
+    }
+    public static void pilihan(int num,int k) throws IOException {
+        switch(num) {
+            case 1:
+                System.out.println("\n");
+                greedy(num,k);
+                break;
+            case 2:
+                System.out.println("\n");
+                greedy(num,k);
+                break;
+            case 3:
+                System.out.println("\n");
+                greedy(num,k);
+                break;
+            case 0:
+                System.out.println("Exit");
+                System.out.println("Tekan enter untuk exit");
+                System.in.read();
+                break;
+            default:
+                System.out.println("inputan salah");
+        }
+    }
+    public static void clrscr() {  
+    System.out.print("\033[H\033[2J");  
+    System.out.flush();  
+   }
+    
+}
